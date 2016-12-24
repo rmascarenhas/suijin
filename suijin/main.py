da96586463772aa8881d63f6721ea076be23948f
Renato Mascarenhas <renato@fastmail.jp>
2016-12-23 22:07:55 -0200

    Run the flow direction algorithm if requested on the command line.

--- suijin/main.py
+++ suijin/main.py
@@ -5,12 +5,15 @@ Most often, this module is not called directly but instead invoked via the
 """
 
 import cli
+import hydro
 
 def main():
     arguments = cli.run()
-    print('Reading TIF from: {}'.format(arguments.input))
-    print('Saving output to: {}'.format(arguments.output))
-    print('Applying algorithm: {}'.format(arguments.algorithm))
+
+    if arguments.algorithm == 'direction':
+        algo = hydro.Direction(arguments.input)
+
+    algo.run(arguments.output)
 
 # entry point of execution: if invoked from `bin/suijin`, run the `main`
 # function defined above
